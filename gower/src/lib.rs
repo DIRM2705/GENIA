3c217ed0abbc9db3afce4906de406ada16e2385e
DIRM2705 <dirm2705@gmail.com>
2025-12-09 14:33:05 -0600

    Optimize paralell algorithm

--- gower/src/lib.rs
+++ gower/src/lib.rs
@@ -1,22 +1,24 @@
+mod student_data;
+
 #[pyo3::pymodule]
 mod gower_distance {
+    use crate::student_data::StudentData;
     use numpy::ndarray;
-    use polars::prelude::AnyValue;
-    use polars::prelude::*;
-    use pyo3::PyResult;
-    use pyo3::exceptions::*;
     use pyo3::prelude::*;
     use pyo3_polars::PyDataFrame;
     use std::cmp::{max, min};
-    use std::sync::{Arc, Mutex};
+    use std::sync::mpsc::Sender;
+    use std::sync::{Arc, Mutex, mpsc};
     use std::thread;
+    use std::time::Duration;
+    use std::vec;
 
     #[pyclass]
     struct GowerMatrix {
-        data: ndarray::Array<f64, ndarray::Dim<[usize; 1]>>,
+        data: ndarray::Array<f64, ndarray::Dim<[usize; 1]>>, //1D array to store upper triangular matrix values
         #[pyo3(get)]
         size: usize,
-        row_starts: Vec<usize>,
+        row_starts: Vec<i32>,
     }
 
     #[pymethods]
@@ -27,121 +29,60 @@ mod gower_distance {
 
             //The array only needs to store the upper triangular matrix thus, its max size is n*(n+1)/2 according
             //to the formula for the sum of the first n natural numbers
-            let array_size = size * (size + 1) / 2; //max size of the array to store the upper triangular matrix
+            let array_size = size * (size - 1) / 2; //max size of the array to store the upper triangular matrix
 
             //Precompute the starting indices of each row in the 1D array
             let mut row_starts = Vec::with_capacity(size);
-            row_starts.push(0);
-            for i in 1..size {
-                let start_index = row_starts[i - 1] + size - 2*i + i;
+            let n = size as i32;
+            row_starts.push(-1);
+            for i in 1..size as i32 {
+                let start_index = row_starts[(i - 1) as usize] - 1 + n - i;
                 row_starts.push(start_index);
             }
             return GowerMatrix {
                 data: ndarray::Array::zeros((array_size,)), //Initialize the array with zeros
-                size, //number of rows/columns in the square matrix
-                row_starts,
+                size,       //number of rows/columns in the square matrix
+                row_starts, //starting indices of each virtual row representation
             };
+        }
+
+        fn get_indices(&self, index: usize) -> (usize, usize) {
+            /*
+               Given a linear index in the 1D array representation of the upper triangular matrix,
+               return the corresponding (i, j) indices in the 2D matrix.
+            */
+            let p = index as f64;
+            let n = self.size as f64;
+            assert!(index < self.data.len(), "Index out of bounds");
+            let b = 2.0 * n - 3.0; //Coefficient for the quadratic equation
+            let c = 8.0 * (p - n + 2.0); //Constant term for the quadratic equation
+
+            //Solve the quadratic equation to find the row index i
+            let i = ((b - (b * b - c).sqrt()) / 2.0).ceil() as usize;
+            //Calculate the column index j
+            let j = (p - self.row_starts[i] as f64) as usize;
+            return (i, j);
         }
 
         fn get(&self, i1: usize, j1: usize) -> f64 {
             assert!(i1 < self.size && j1 < self.size, "Index out of bounds");
+            if i1 == j1 {
+                return 0.0; //Distance to self is zero
+            }
+            
+            //else
             //Get the value at position (i,j) in the Gower matrix
             let i = min(i1, j1);
             let j = max(i1, j1);
-            let index = self.row_starts[i] + j; //Calculate the index in the 1D array
-            return self.data[index];
+            let index = self.row_starts[i] + j as i32; //Calculate the index in the 1D array
+            return self.data[index as usize];
         }
 
-        fn set(&mut self, i1: usize, j1: usize, value: f64) {
-            assert!(i1 < self.size && j1 < self.size, "Index out of bounds");
-            //Set the value at position (i,j) in the Gower matrix
-            let i = min(i1, j1);
-            let j = max(i1, j1);
-            let index = self.row_starts[i] + j; //Calculate the index in the 1D array
-            self.data[index] = value; //Set the value
+        fn set(&mut self, index : usize, value: f64) {
+            assert!(index < self.data.len(), "Index out of bounds");
+            self.data[index] = value; //Set the value in the 1D array
         }
     }
-    struct StudentData {
-        data_frame: DataFrame,
-        rows: usize,
-        columns: usize,
-        dtypes: Vec<DataType>,
-    }
-
-    impl StudentData {
-        fn new(data_frame: DataFrame) -> Self {
-            let column_count = data_frame.width();
-            let row_count = data_frame.height();
-            let dtypes = data_frame
-                .get_columns()
-                .iter()
-                .map(|col| col.dtype().clone())
-                .collect();
-            StudentData {
-                columns: column_count,
-                rows: row_count,
-                dtypes,
-                data_frame,
-            }
-        }
-
-        fn get_distance_between_students(&self, s1_index: usize, s2_index: usize) -> PyResult<f64> {
-            //Calculate the Gower distance between two students
-            let mut total_distance = 0.0;
-            let row_1 = self.data_frame.get(s1_index);
-            let row_2 = self.data_frame.get(s2_index);
-
-            if row_1.is_none() || row_2.is_none() {
-                return Err(PyErr::new::<PyIndexError, _>("Student index out of bounds"))?;
-            }
-
-            let s1_row = row_1.unwrap();
-            let s2_row = row_2.unwrap();
-            let ranks = self.get_column_ranks();
-
-            for i in 0..self.columns {
-                let student_1 = s1_row.get(i).unwrap();
-                let student_2 = s2_row.get(i).unwrap();
-                let distance = match self.dtypes[i] {
-                    //Handle numerical columns
-                    DataType::Float64 => get_distance_numerical(student_1, student_2, ranks[i]),
-                    //Handle categorical columns
-                    DataType::Categorical(_, _) => get_distance_categorical(student_1, student_2),
-                    _ => {
-                        //Error handling for unsupported data types
-                        return Err(PyErr::new::<PyTypeError, _>(format!(
-                            "Unsupported data type in column {}",
-                            self.data_frame.get_column_names()[i]
-                        )))?;
-                    }
-                };
-                total_distance += distance;
-            }
-            return Ok(total_distance);
-        }
-
-        fn get_column_ranks(&self) -> Vec<f64> {
-            //Calculate the rank (max - min) for each numerical column
-            let mut ranks: Vec<f64> = vec![];
-            for col in self.data_frame.get_columns() {
-                match col.dtype() {
-                    DataType::Float64 => {
-                        let series = col.f64().unwrap();
-                        let min_value = series.min().unwrap_or(0.0);
-                        let max_value = series.max().unwrap_or(0.0);
-                        let rank = max_value - min_value;
-                        ranks.push(rank);
-                    }
-                    _ => {
-                        //For non-numerical columns, push a dummy value (not used)
-                        ranks.push(0.0);
-                    }
-                }
-            }
-            return ranks;
-        }
-    }
-
     #[pyfunction]
     fn make_gower_matrix(pydf: PyDataFrame) -> PyResult<GowerMatrix> {
         /*
@@ -154,103 +95,120 @@ mod gower_distance {
 
         let students_data = Arc::new(Mutex::new(StudentData::new(pydf.into())));
         let student_count = students_data.lock().unwrap().rows;
-        let gower_matrix = Arc::new(Mutex::new(GowerMatrix::new(student_count)));
-        let threads = create_threads(Arc::clone(&students_data), Arc::clone(&gower_matrix));
+        let mut gower_matrix = GowerMatrix::new(student_count);
+        let (tx, rx) = mpsc::channel();
+        let threads = create_threads(Arc::clone(&students_data), &gower_matrix, tx);
 
-        // Wait for all threads to finish
+        // Wait for all threads to finish and collect results
+        while let Ok((idx, distance)) = rx.recv_timeout(Duration::from_millis(500)) {
+            gower_matrix.set(idx, distance);
+        }
         for handle in threads {
             handle.join().expect("Thread panicked");
         }
 
-        // Unwrap the Arc<Mutex<GowerMatrix>> to return the GowerMatrix
-        let matrix = Arc::try_unwrap(gower_matrix)
-            .map_err(|_| PyErr::new::<PyRuntimeError, _>("Failed to unwrap Arc"))?
-            .into_inner()
-            .map_err(|_| {
-                PyErr::new::<PyRuntimeError, _>("Failed to lock Mutex")
-            })?;
-
-        return Ok(matrix);
+        return Ok(gower_matrix);
     }
 
     fn create_threads(
         student_data: Arc<Mutex<StudentData>>,
-        matrix: Arc<Mutex<GowerMatrix>>,
+        gower_matrix: &GowerMatrix,
+        sender: Sender<(usize, f64)>,
     ) -> Vec<thread::JoinHandle<()>> {
         let mut threads: Vec<thread::JoinHandle<()>> = vec![]; //Vector to hold thread handles
-        let max_threads = num_cpus::get(); //Get the number of available CPU cores
         let rows = student_data.lock().unwrap().rows;
-        let students_per_thread = rows / max_threads + 1; //Calculate number of students per thread
-        
+        //Determine the number of threads to use
+        let max_threads = min(rows, num_cpus::get() - 2);
+
+        //Calculate number of operations between students per thread
+        let ops_per_thread = (rows * (rows - 1) / 2) / max_threads;
+
         //Create threads to compute Gower distances in parallel
-        let mut thread_index = 0;
-        loop
-        {
-            let student_index = thread_index * students_per_thread;
-            if student_index >= rows {
+        for thread_index in 0..max_threads {
+            let begin_index = thread_index * ops_per_thread;
+            if begin_index >= rows {
                 break;
             }
             threads.push(create_thread(
-                student_index,
-                students_per_thread,
+                begin_index,
+                ops_per_thread,
+                gower_matrix,
                 Arc::clone(&student_data),
-                Arc::clone(&matrix),
+                sender.clone(),
             ));
-
-            thread_index += 1;
         }
 
         return threads;
     }
 
     fn create_thread(
-        mut student_index: usize,
-        students_per_thread: usize,
+        mut op_idx: usize,
+        mut ops_per_thread: usize,
+        gower_matrix: &GowerMatrix,
         student_data: Arc<Mutex<StudentData>>,
-        matrix: Arc<Mutex<GowerMatrix>>,
+        sender: Sender<(usize, f64)>,
     ) -> thread::JoinHandle<()> {
+        let (mut i, mut j) = gower_matrix.get_indices(op_idx);
         return thread::spawn(move || {
             let student_data_guard = student_data.lock().unwrap();
-            let mut matrix_guard = matrix.lock().unwrap();
-            let rows = student_data_guard.rows;
-            let columns = student_data_guard.columns;
-            let end_index = min(student_index + students_per_thread, rows);
-            while student_index < end_index
+            let total_categories = student_data_guard.category_count;
+            let mut i_num_distances = student_data_guard.get_row_numerical(i);
+            let mut i_cat_distances = student_data_guard.get_row_categorical(i);
+            let ranks = student_data_guard.ranks.clone();
+            drop(student_data_guard); //Release the lock before entering the loop
+
+            while ops_per_thread > 0 
             {
-                for j in (student_index + 1)..rows {
-                    let distance = match student_data_guard.get_distance_between_students(student_index, j) {
-                        Ok(dist) => 1.0 - dist / columns as f64,
-                        Err(_) => return,
-                    };
-                    matrix_guard.set(student_index, j, distance);
+                let student_data_guard = student_data.lock().unwrap();
+                let j_num_distances = student_data_guard.get_row_numerical(j);
+                let j_cat_distances = student_data_guard.get_row_categorical(j);
+                drop(student_data_guard); //Release the lock before calculating distances
+
+                //Calculate distances for numerical columns
+                let num_distance = calculate_distances_numerical(&i_num_distances, &j_num_distances, &ranks);
+                let cat_distance = calculate_distances_categorical(&i_cat_distances, &j_cat_distances);
+
+                let total_distance = 1.0 - (num_distance + cat_distance)/total_categories;
+                sender.send((op_idx, total_distance)).unwrap();
+
+                //Move to the next pair (i, j)
+                ops_per_thread -= 1;
+                op_idx += 1;
+                
+                if j + 1 >= student_data.lock().unwrap().rows {
+                    i += 1;
+                    let student_data_guard = student_data.lock().unwrap();
+                    i_num_distances = student_data_guard.get_row_numerical(i);
+                    i_cat_distances = student_data_guard.get_row_categorical(i);
+                    drop(student_data_guard); //Release the lock before continuing
+                    j = i+1;
                 }
-                student_index += 1;
+                else 
+                {
+                    j += 1;
+                }
             }
         });
     }
 
-    fn get_distance_categorical(s1_value: &AnyValue, s2_value: &AnyValue) -> f64 {
-        let s1_value = s1_value.to_string();
-        let s2_value = s2_value.to_string();   
-        //Get gower similarity between two categorical values
-        if s1_value == s2_value {
-            return 1.0;
-        } else {
-            return 0.0;
+    pub fn calculate_distances_numerical(s1_row: &Vec<f64>, s2_row : &Vec<f64>, ranks : &Vec<f64>) -> f64 {
+        //Get gower similarity bewteen two numerical values
+        let mut distance = 0.0;
+        for k in 0..s1_row.len() {
+            let diff = (s1_row[k] - s2_row[k]).abs();
+            distance += 1.0 - diff / ranks[k];
         }
+        return distance;
     }
 
-    fn get_distance_numerical(s1_value: &AnyValue, s2_value: &AnyValue, rank: f64) -> f64 {
-        let s1_value = match s1_value {
-            AnyValue::Float64(v) => v,
-            _ => panic!("Expected Float64 value"),
-        };
-        let s2_value = match s2_value {
-            AnyValue::Float64(v) => v,
-            _ => panic!("Expected Float64 value"),
-        };
-        //Get gower similarity bewteen two numerical values
-        let diff = 1.0 - ((s1_value - s2_value).abs() / rank);
-        return diff;
+    pub fn calculate_distances_categorical(s1_row: &Vec<String>, s2_row : &Vec<String>) -> f64 {
+        //Get gower similarity bewteen two categorical values
+        let mut distance = 0.0;
+        for k in 0..s1_row.len() {
+            if s1_row[k] == s2_row[k] {
+                distance += 1.0;
+            }
+        }
+        return distance
     }
 }
