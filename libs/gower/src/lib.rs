a43ffdf340fe850cd460e4a32f593ec5f4eb77d6
DIRM2705 <dirm2705@gmail.com>
2026-01-08 12:44:28 -0600

    Update to compute only distances between two points

--- libs/gower/src/lib.rs
+++ libs/gower/src/lib.rs
@@ -1,137 +1,74 @@
-mod student_data;
+use hypergraph::Student;
 
-use crate::student_data::StudentData;
-use polars::prelude::DataFrame;
-use std::sync::mpsc::Sender;
-use std::sync::{Arc, mpsc};
-use std::thread;
-use std::time::Duration;
-use std::vec;
-use symmetric_matrix::SymmetricMatrix;
+const TOTAL_ATTRIBUTES: f64 = 17.0; //Total number of attributes per student
+const NUMERICAL_ATTRIBUTES: usize = 10; //Number of numerical attributes
+const MI_ATTRIBUTES: usize = 2; //Number of multiple intelligences attributes
+const NDD_ATTRIBUTES: usize = 5; //Number of neurodevelopmental disorder attributes
 
-pub fn make_matrix(dataframe: DataFrame) -> Result<SymmetricMatrix, Box<dyn std::error::Error>> {
-    /*
-       Gower distance calculation
-       ARGS:
-           df: DataFrame that includes both numerical and categorical variables
-       RETURNS:
-           struct SymmetricMatrix that includes the gower distance matrix and its size
-    */
+pub fn calculate_gower_distance(s1: &Student, s2: &Student, ranks: &Vec<f64>) -> f64 {
+    //Calculate Gower distance between two students
+    let mut distance = 0.0;
 
-    let students_data = Arc::new(StudentData::new(dataframe));
-    let student_count = students_data.rows;
-    let mut gower_matrix = SymmetricMatrix::new(student_count);
-    let (tx, rx) = mpsc::channel();
-    let threads = create_threads(Arc::clone(&students_data), &gower_matrix, tx);
+    //Numerical attributes
+    distance += calculate_distances_numerical(&s1, &s2, ranks);
 
-    // Wait for all threads to finish and collect results
-    while let Ok((idx, distance)) = rx.recv_timeout(Duration::from_millis(500)) {
-        gower_matrix.data[idx] = distance;
-    }
-    for handle in threads {
-        handle.join().expect("Thread panicked");
-    }
+    //Categorical attributes
+    distance += calculate_distances_categorical(&s1, &s2);
 
-    return Ok(gower_matrix);
+    return distance/TOTAL_ATTRIBUTES;
 }
 
-fn create_threads(
-    student_data: Arc<StudentData>,
-    gower_matrix: &SymmetricMatrix,
-    sender: Sender<(usize, f64)>,
-) -> Vec<thread::JoinHandle<()>> {
-    let mut threads: Vec<thread::JoinHandle<()>> = vec![]; //Vector to hold thread handles
-    let rows = student_data.rows;
-    //Determine the number of threads to use
-    let max_threads = num_cpus::get() - 2;
-
-    //Calculate number of operations between students per thread
-    let ops_per_thread = (rows * (rows - 1) / 2) / max_threads + 1;
-
-    //Create threads to compute Gower distances in parallel
-    for thread_index in 0..max_threads {
-        let begin_index = thread_index * ops_per_thread;
-        if begin_index >= gower_matrix.data.len() {
-            break;
-        }
-        threads.push(create_thread(
-            begin_index,
-            ops_per_thread,
-            gower_matrix,
-            Arc::clone(&student_data),
-            sender.clone(),
-        ));
-    }
-
-    return threads;
-}
-
-fn create_thread(
-    mut op_idx: usize,
-    mut ops_per_thread: usize,
-    gower_matrix: &SymmetricMatrix,
-    student_data: Arc<StudentData>,
-    sender: Sender<(usize, f64)>,
-) -> thread::JoinHandle<()> {
-    let (mut i, mut j) = gower_matrix.get_indices(op_idx);
-    let size = gower_matrix.size;
-    return thread::spawn(move || {
-        let total_categories = student_data.category_count;
-        let mut i_num_distances = student_data.get_row_numerical(i);
-        let mut i_cat_distances = student_data.get_row_categorical(i);
-        let ranks = student_data.ranks.clone();
-
-        while ops_per_thread > 0 {
-            let j_num_distances = student_data.get_row_numerical(j);
-            let j_cat_distances = student_data.get_row_categorical(j);
-            //Calculate distances for numerical columns
-            let num_distance =
-                calculate_distances_numerical(&i_num_distances, &j_num_distances, &ranks);
-            let cat_distance = calculate_distances_categorical(&i_cat_distances, &j_cat_distances);
-
-            let total_distance = 1.0 - (num_distance + cat_distance) / total_categories;
-            sender.send((op_idx, total_distance)).unwrap();
-
-            //Move to the next pair (i, j)
-            ops_per_thread -= 1;
-            op_idx += 1;
-
-            if j + 1 >= size {
-                i += 1;
-                if i >= size - 1 {
-                    break; //All pairs processed
-                }
-                i_num_distances = student_data.get_row_numerical(i);
-                i_cat_distances = student_data.get_row_categorical(i);
-                j = i + 1;
-            } else {
-                j += 1;
-            }
-        }
-    });
-}
-
-pub fn calculate_distances_numerical(
-    s1_row: &Vec<f64>,
-    s2_row: &Vec<f64>,
-    ranks: &Vec<f64>,
-) -> f64 {
+fn calculate_distances_numerical(s1 : &Student, s2: &Student, ranks: &Vec<f64>) -> f64 {
     //Get gower similarity bewteen two numerical values
     let mut distance = 0.0;
-    for k in 0..s1_row.len() {
+
+    let mut s1_row = vec![
+        s1.be,
+        s1.ee,
+        s1.ce,
+        s1.autonomous_motivation,
+        s1.competitive_motivation,
+        s1.relationship_motivation,
+    ];
+    s1_row.extend(s1.vark_scores.iter()); //Add VARK scores
+
+    let mut s2_row = vec![
+        s2.be,
+        s2.ee,
+        s2.ce,
+        s2.autonomous_motivation,
+        s2.competitive_motivation,
+        s2.relationship_motivation,
+    ];
+    s2_row.extend(s2.vark_scores.iter()); //Add VARK scores
+
+    for k in 0..NUMERICAL_ATTRIBUTES {
         let diff = (s1_row[k] - s2_row[k]).abs();
         distance += 1.0 - diff / ranks[k];
     }
     return distance;
 }
 
-pub fn calculate_distances_categorical(s1_row: &Vec<String>, s2_row: &Vec<String>) -> f64 {
+fn calculate_distances_categorical(s1 : &Student, s2: &Student) -> f64 {
     //Get gower similarity bewteen two categorical values
     let mut distance = 0.0;
-    for k in 0..s1_row.len() {
-        if s1_row[k] == s2_row[k] {
-            distance += 1.0;
-        }
+    let s1_row = vec![
+        s1.mi_order[0],
+        s1.mi_order[1]
+    ];
+    let s2_row = vec![
+        s2.mi_order[0],
+        s2.mi_order[1],
+    ];
+
+    for k in 0..MI_ATTRIBUTES {
+        distance += (s1_row[k] == s2_row[k]) as u32 as f64;
+    }
+
+    for k in 0..NDD_ATTRIBUTES
+    {
+        //Compare ndd bit by bit (8 bits)
+        distance += (s1.ndd & s2.ndd & (1 << k)) as f64;
     }
     return distance;
 }
