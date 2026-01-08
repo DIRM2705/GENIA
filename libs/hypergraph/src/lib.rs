e08fccddb3eba9fe6fff9d14d29e9b1b17f859f6
DIRM2705 <dirm2705@gmail.com>
2026-01-08 12:45:13 -0600

    Update Studnent struct

--- libs/hypergraph/src/lib.rs
+++ libs/hypergraph/src/lib.rs
@@ -8,12 +8,22 @@ pub struct Hypergraph
 pub struct Hyperedge
 {
     name_id : String,
-    nodes : HashSet<Node>,
+    nodes : HashSet<Student>,
 }
 
-pub struct Node
+pub struct Student
 {
-    id : usize
+    pub id : usize,
+    pub ndd: u8, //Bit map for neurodevelopmental disorders 
+    pub mi_order: [u8; 8], //Order of multiple intelligences
+    pub vark_scores: [f64; 4], //VARK learning style scores
+    pub be : f64, //Behavioral engagement percentage
+    pub ee: f64, //Emotional engagement percentage
+    pub ce : f64, //Cognitive engagement percentage
+    pub autonomous_motivation : f64, //Autonomous motivation percentage
+    pub competitive_motivation : f64, //Competitive motivation percentage
+    pub relationship_motivation : f64, //Relationship motivation percentage
+    pub gpa : f64,
 }
 
 
@@ -47,7 +57,7 @@ impl Hyperedge
     }
 
     //Create a hyperedge with a given name_id and set of nodes
-    pub fn new(name_id: String, nodes: HashSet<Node>) -> Self 
+    pub fn new(name_id: String, nodes: HashSet<Student>) -> Self 
     {
         Hyperedge 
         {
