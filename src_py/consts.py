ccb7a96a9bf83cffb9da512151a464af5d671cf9
DIRM2705 <dirm2705@gmail.com>
2026-01-26 10:54:54 -0600

    Add consts file

---
+++ src_py/consts.py
@@ -0,0 +1,64 @@
+VISUAL_IDX = 0
+AURAL_IDX = 1
+READWRITE_IDX = 2
+KINESTHETIC_IDX = 3
+NDD_LIST = ['disgrafía', 'discalculia', 'dislexia', 'tda o tdah', 'tea']
+
+VISUAL_ANSWERS = [
+        "dibujaría un mapa?",
+        "le dibujen un mapa?",
+        "le mostraría los detalles en un mapa del mundo?",
+        "revisa las imágenes del libro de recetas en busca de ideas?",
+        "les muestra diapositivas y fotografías de los parques?",
+        "se ve de buena calidad",
+        "representaciones visuales – imágenes, diagramas, tablas",
+        "pictionary",
+        "observa la palabra en su mente y escoge la opción que se ve mejor?",
+        "se ve ""ok""",
+        "ver un tráiler de la película",
+        "diagramas de flujo, tablas y diapositivas?"
+    ]
+    
+AURAL_ANSWERS = [
+        "daría instrucciones verbalmente?",
+        "le den las direcciones por llamada telfónica?",
+        "lo llamaría de inmediato y le contaría al respecto?",
+        "busca consejo de alguien más?",
+        "les da una charla/exposición sobre parques nacionales?",
+        "la recomendación de un amigo",
+        "escuchar la explicación de alguien más",
+        "llamaría por teléfono a un amigo y le preguntaría sobre el programa?",
+        "la pronuncia lenta y articuladamente en su mente?",
+        "la opinión de un amigo",
+        "qué sus amigos hablen de la película",
+        "discusión en grupos, conferencias?"
+    ]
+    
+READ_WRITE_ANSWERS = [
+        "escribiría las direcciones en un papel?",
+        "le escriban las direcciones?",
+        "le mandaría una copia impresa del itinerario?",
+        "busca en un libro específico en el que hay una buena receta?",
+        "les da un libro sobre parques nacionales?",
+        "leer más detalles del producto",
+        "instrucciones escritas",
+        "scrabble",
+        "leería el manual que viene en el programa?",
+        "busca la escritura correcta en el diccionario?",
+        "hojear partes del libro",
+        "leer una reseña",
+        "hojas de trabajo o libros de texto?"
+    ]
+    
+KINESTHETIC_ANSWERS = [
+        "la recogería de su hotel en su auto?",
+        "lo recoja del hotel su amigo en su auto?",
+        "cocina algo sin necesidad de instrucciones?",
+        "los lleva a un parque nacional?",
+        "haciendo esta nueva actividad",
+        "caras y gestos",
+        "le pediría a un amigo que le enseñe a usarlo?",
+        "escribe ambas versiones?",
+        "usar la copia del libro de un amigo",
+        "investigación de campo, laboratorios, sesiones prácticas?"
+    ]
