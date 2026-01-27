3e7563c9c5494a1c16b5cacbadb8cefef1f3b1f6
DIRM2705 <dirm2705@gmail.com>
2026-01-27 12:19:12 -0600

    Update Characteristic taking

--- libs/group_enhancer/src/lib.rs
+++ libs/group_enhancer/src/lib.rs
@@ -8,8 +8,53 @@ mod group_enhancer {
     use symmetric_matrix::SymmetricMatrix;
 
     #[pyclass]
-    struct PyCharacteristicType {
-        inner: CharacteristicType
+    enum PyCharacteristicType {
+        CChronotype,
+        ADHD,
+        AUTSIM,
+        DISLEXIA,
+        DISGRAFIA,
+        DISCALCULIA,
+        MI1,
+        MI2,
+        MI3,    
+        VarkVisual,
+        VarkAural,
+        VarkRW,
+        VarkKinesthetic,
+        BE,
+        EE,
+        CE,
+        AM,
+        CM,
+        RM
+    }
+
+    impl PyCharacteristicType
+    {
+        fn to_characteristic_type(&self, value: u8) -> CharacteristicType {
+            return match self {
+                PyCharacteristicType::CChronotype => CharacteristicType::Chronotype(value),
+                PyCharacteristicType::ADHD => CharacteristicType::ADHD,
+                PyCharacteristicType::AUTSIM => CharacteristicType::AUTSIM,
+                PyCharacteristicType::DISLEXIA => CharacteristicType::DISLEXIA,
+                PyCharacteristicType::DISGRAFIA => CharacteristicType::DISGRAFIA,
+                PyCharacteristicType::DISCALCULIA => CharacteristicType::DISCALCULIA,
+                PyCharacteristicType::MI1 => CharacteristicType::MI1(value),
+                PyCharacteristicType::MI2 => CharacteristicType::MI2(value),
+                PyCharacteristicType::MI3 => CharacteristicType::MI3(value),    
+                PyCharacteristicType::VarkVisual => CharacteristicType::VarkVisual(value),
+                PyCharacteristicType::VarkAural => CharacteristicType::VarkAural(value),
+                PyCharacteristicType::VarkRW => CharacteristicType::VarkRW(value),
+                PyCharacteristicType::VarkKinesthetic => CharacteristicType::VarkKinesthetic(value),
+                PyCharacteristicType::BE => CharacteristicType::BE(value),
+                PyCharacteristicType::EE => CharacteristicType::EE(value),
+                PyCharacteristicType::CE => CharacteristicType::CE(value),
+                PyCharacteristicType::AM => CharacteristicType::AM(value),
+                PyCharacteristicType::CM => CharacteristicType::CM(value),
+                PyCharacteristicType::RM => CharacteristicType::RM(value),
+            }
+        }
     }
 
     #[pyclass]
@@ -26,10 +71,11 @@ mod group_enhancer {
             };
         }
 
-        fn add_students_to_characteristic(&mut self, characteristic : &PyCharacteristicType, student_ids: Vec<usize>) {
+        fn add_students_to_characteristic(&mut self, student_ids: Vec<usize>, characteristic : &PyCharacteristicType, value: Option<u8>) {
             for student_id in student_ids
             {
-                self.inner.add_student_to_characteristic(&characteristic.inner, student_id);
+                let characteristic = characteristic.to_characteristic_type(value.unwrap_or(0));
+                self.inner.add_student_to_characteristic(&characteristic, student_id);
             }
         }
 
