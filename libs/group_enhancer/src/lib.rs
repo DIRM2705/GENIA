2c98d883e9c04d06cacf9c2c589f42d22a433808
DIRM2705 <dirm2705@gmail.com>
2026-02-28 13:48:58 -0600

    Update hypergraph names

--- libs/group_enhancer/src/lib.rs
+++ libs/group_enhancer/src/lib.rs
@@ -1,30 +1,28 @@
 #[pyo3::pymodule]
 mod group_enhancer {
-    //use gower::calculate_gower_distance;
+    use gower::calculate_gower_matrix;
     use hypergraph::Hypergraph;
     use pyo3::prelude::*;
+    use pyo3_polars::PyDataFrame;
     use symmetric_matrix::SymmetricMatrix;
     
     #[pyclass]
-    struct CharacteristicHG {
+    struct GroupHG {
         inner: Hypergraph<u32>,
     }
 
     #[pymethods]
-    impl CharacteristicHG {
+    impl GroupHG {
 
         #[new]
         fn new(num_students: usize) -> Self {
-            return CharacteristicHG {
+            return GroupHG {
                 inner: Hypergraph::new(num_students),
             };
         }
 
-        fn add_students_to_characteristic(&mut self, student_ids: Vec<usize>, characteristic : &str) {
-            for student_id in student_ids
-            {
-                self.inner.add_id_to_hyperedge(student_id, &characteristic.to_string());
-            }
+        fn add_students_to_hyperedge(&mut self, students : PyDataFrame, characteristic : &str) {
+            self.inner.add_students_to_hyperedge(students.into(), &characteristic.to_string());
         }
 
         fn print(&self) {
