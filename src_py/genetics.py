c22339387cc7c9b681441a0583f60f746729f971
DIRM2705 <dirm2705@gmail.com>
2026-03-01 13:43:30 -0600

    Update use genetics library

--- src_py/genetics.py
+++ src_py/genetics.py
@@ -1,24 +1,4 @@
-from dataclasses import dataclass
-from group_enhancer import PyHypergraph
+from group_enhancer import PyIndividual
 
-@dataclass
-class individual:
-    _genome : PyHypergraph
-    _fitness : float
-    
-    def __init__(self, genome : PyHypergraph):
-        self._genome = genome
-        self._fitness = 0.0
-        
-    def cross(self, other : 'individual') -> 'individual':
-        # Implementar el cruce entre dos individuos para generar un nuevo individuo
-        pass
-    
-    def mutate(self, mutation_rate : float):
-        # Implementar la mutación del individuo con una tasa de mutación dada
-        pass
-    
-    def fit(self):
-        # Calcular la aptitud del individuo utilizando una función de aptitud dada
-        self._fitness = 0.0
+
     
