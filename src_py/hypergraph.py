ffa455f39028605bad53f4225c250d5762ac80b5
DIRM2705 <dirm2705@gmail.com>
2026-03-01 16:06:15 -0600

    Remove print statements

--- src_py/hypergraph.py
+++ src_py/hypergraph.py
@@ -5,7 +5,6 @@ def add_to_continous(df : pl.DataFrame, hg : CharacteristicHG, col_name : str, b
     min_val = 0
     for i in range(5):
         students = df.filter((pl.col(col_name) > min_val) & (pl.col(col_name) <= min_val + 0.2)).select("Id").to_series().to_list()
-        print("Añadiendo estudiantes con {} {}: {}".format(col_name, i+1, students))
         for student in students:
             #Insertar al estudiante en el hipergrafo de la característica correspondiente,
             #en la hiperarista correspondiente a su rango de valor de la característica
@@ -15,7 +14,6 @@ def add_to_continous(df : pl.DataFrame, hg : CharacteristicHG, col_name : str, b
         
 def add_to_discrete(df : pl.DataFrame, hg : CharacteristicHG, col_name : str, idx :int):
     students = df.filter(pl.col(col_name).is_in([1,2])).select("Id").to_series().to_list()
-    print("Añadiendo estudiantes con {} {}: {}".format(col_name, 1, students))
     for student in students:
         #Insertar al estudiante en el hipergrafo de la característica correspondiente,
         #en la hiperarista correspondiente a su valor de la característica
