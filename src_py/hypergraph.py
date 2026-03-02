fbee1aaec8c75f7c58e1d6f55c6086e3bd6b4e3b
DIRM2705 <dirm2705@gmail.com>
2026-03-01 19:38:48 -0600

    Add function create hypergraph

--- src_py/hypergraph.py
+++ src_py/hypergraph.py
@@ -1,4 +1,5 @@
 import polars as pl
+from consts import *
 from group_enhancer import CharacteristicHG
 
 def add_to_continous(df : pl.DataFrame, hg : CharacteristicHG, col_name : str, base_idx :int):
@@ -18,3 +19,18 @@ def add_to_discrete(df : pl.DataFrame, hg : CharacteristicHG, col_name : str, id
         #Insertar al estudiante en el hipergrafo de la característica correspondiente,
         #en la hiperarista correspondiente a su valor de la característica
         hg.add_to_hyperedge(student, idx)
+
+def create_hypergraph(df : pl.DataFrame) -> CharacteristicHG:
+    hg = CharacteristicHG(df.height)
+    add_to_continous(df, hg, "AMotiv", AM_BASE_IDX) #Añadir a caracteristicas de motivación autónoma
+    add_to_continous(df, hg, "RMotiv", RM_BASE_IDX) #Añadir a caracteristicas de motivación de relación
+    add_to_continous(df, hg, "CMotiv", CM_BASE_IDX) #Añadir a caracteristicas de motivación de competencia
+    add_to_continous(df, hg, "BEngage", BE_BASE_IDX) #Añadir a caracteristicas de compromiso conductual
+    add_to_continous(df, hg, "EEngage", EE_BASE_IDX) #Añadir a caracteristicas de compromiso emocional
+    add_to_continous(df, hg, "CEngage", CE_BASE_IDX) #Añadir a caracteristicas de compromiso cognitivo
+
+    for i in range(len(VARK_COLUMNS)):
+        add_to_discrete(df, hg, VARK_COLUMNS[i], VARK_BASE_IDX + i) #Añadir a caracteristicas de estilo de aprendizaje 
+        
+    for i in range(len(INTELLIGENCE_BY_INDEX)):
+        add_to_discrete(df, hg, INTELLIGENCE_BY_INDEX[i], INTELLIGENCE_BASE_IDX + i) #Añadir a caracteristicas de inteligencia
