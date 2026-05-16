170cb7fb66497cb99c48292f0f7a945676907b15
DIRM2705 <dirm2705@gmail.com>
2026-05-16 07:34:38 -0600

    Añade biblioteca de creación de hipergrafos

---
+++ libs/hypergraph/src/bitmap.rs
@@ -0,0 +1,196 @@
+/*
+    Módulo para crear los bitmaps de tamaño variable requeridos por las hiperaristas
+    Se implementan para tamaños variables, principalmente:
+    - 8 bits
+    - 16 bits
+    - 32 bits
+    - 64 bits
+    - 128 bits
+
+    Se utiliza un arreglo de bytes por si se necesitaran bitmaps de tamaño superior
+ */
+
+use std::io::{Result, Error};
+
+#[typetag::serde(tag = "BitmapLen")]
+pub trait BitmapLen {
+    fn get_size_bits(&self) -> usize;
+    fn set_bit(&mut self, index: usize) -> Result<(), String>; // Method to set a specific bit in the bitmap
+    fn get_bit(&self, index: usize) -> Result<bool>; // Method to get the value of a specific bit in the bitmap
+}
+
+#[typetag::serde]
+impl BitmapLen for u8 {
+    fn get_size_bits(&self) -> usize {
+        return 8;
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= 8 {
+            return Err("Index out of bounds for u8 bitmap".into());
+        }
+
+        *self |= 1 << index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= 8 {
+            return Err("Index out of bounds for u8 bitmap".into());
+        }
+
+        Ok((*self & (1 << index)) != 0)
+    }
+}
+
+#[typetag::serde]
+impl BitmapLen for u16 {
+    fn get_size_bits(&self) -> usize {
+        return 16;
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= 16 {
+            return Err("Index out of bounds for u16 bitmap".into());
+        }
+
+        *self |= 1 << index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= 16 {
+            return Err("Index out of bounds for u16 bitmap".into());
+        }
+
+        Ok((*self & (1 << index)) != 0)
+    }
+}
+
+#[typetag::serde]
+impl BitmapLen for u32 {
+    fn get_size_bits(&self) -> usize {
+        return 32;
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= 32 {
+            return Err("Index out of bounds for u32 bitmap".into());
+        }
+
+        *self |= 1 << index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= 32 {
+            return Err("Index out of bounds for u32 bitmap".into());
+        }
+
+        Ok((*self & (1 << index)) != 0)
+    }
+}
+
+#[typetag::serde]
+impl BitmapLen for u64 {
+    fn get_size_bits(&self) -> usize {
+        return 64;
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= 64 {
+            return Err("Index out of bounds for u64 bitmap".into());
+        }
+
+        *self |= 1 << index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= 64 {
+            return Err("Index out of bounds for u64 bitmap".into());
+        }
+
+        Ok((*self & (1 << index)) != 0)
+    }
+}
+
+#[typetag::serde]
+impl BitmapLen for u128 {
+    fn get_size_bits(&self) -> usize {
+        return 128;
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= 128 {
+            return Err("Index out of bounds for u128 bitmap".into());
+        }
+        *self |= 1 << index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= 128 {
+            return Err("Index out of bounds for u128 bitmap".into());
+        }
+
+        Ok((*self & (1 << index)) != 0)
+    }
+}
+
+#[typetag::serde]
+impl BitmapLen for Box<[u8]> {
+
+    fn get_size_bits(&self) -> usize {
+        self.len() * 8
+    }
+
+    fn set_bit(&mut self, index: usize) -> Result<(), String> {
+        if index >= self.get_size_bits() {
+            return Err("Index out of bounds for Box<[u8]> bitmap".into());
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+        self[byte_index] |= 1 << bit_index;
+        Ok(())
+    }
+
+    fn get_bit(&self, index: usize) -> Result<bool> {
+        if index >= self.get_size_bits() {
+            return Err("Index out of bounds for Box<[u8]> bitmap".into());
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+        Ok((self[byte_index] & (1 << bit_index)) != 0)
+    }
+}
+
+// Crea un bitmap de tamaño específico
+pub fn make_bitmap_of_len(size_bits: usize) -> Box<dyn BitmapLen> {
+    let size_bytes = (size_bits + 7) / 8; // Calcular el número de bytes mínimo
+    match size_bytes {
+        0..=1 => Box::new(0u8),    
+        2 => Box::new(0u16),       
+        3..=4 => Box::new(0u32),   
+        5..=8 => Box::new(0u64),   
+        9..=16 => Box::new(0u128),
+        _ => Box::new(vec![0u8; size_bytes].into_boxed_slice())
+    }
+}
+
+//Cambiar el tamaño del bitmap, copiando los bits del bitmap original al nuevo bitmap
+pub fn resize_bitmap(bitmap: &Box<dyn BitmapLen>, new_size_bits: usize) -> Result<Box<dyn BitmapLen>, String> {
+    if new_size_bits < bitmap.get_size_bits() {
+        return Ok(*bitmap); // Si el nuevo tamaño es menor, no se hace nada y se devuelve el bitmap original
+    }
+
+    let mut target  = make_bitmap_of_len(new_size_bits);
+    for i in 0..bitmap.get_size_bits() {
+        if bitmap.get_bit(i)? {
+            target.set_bit(i)?;
+        }
+    }
+    return Ok(target);
+}
--- libs/hypergraph/src/lib.rs
+++ libs/hypergraph/src/lib.rs
@@ -1,137 +1,92 @@
-pub trait Hypergraph
-{
-    const ID_IDX : usize = 0;
-    const AM1_IDX : usize = 3;
-    const AM2_IDX : usize = 4;
-    const AM3_IDX : usize = 5;
-    const AM4_IDX : usize = 6;
-    const AM5_IDX : usize = 7;
-    const RM1_IDX : usize = 8;
-    const RM2_IDX : usize = 9;
-    const RM3_IDX : usize = 10;
-    const RM4_IDX : usize = 11;
-    const RM5_IDX : usize = 12;
-    const CM1_IDX : usize = 13;
-    const CM2_IDX : usize = 14;
-    const CM3_IDX : usize = 15;
-    const CM4_IDX : usize = 16;
-    const CM5_IDX : usize = 17;
-    const BE1_IDX : usize = 18;
-    const BE2_IDX : usize = 19;
-    const BE3_IDX : usize = 20;
-    const BE4_IDX : usize = 21;
-    const BE5_IDX : usize = 22;
-    const EE1_IDX : usize = 23;
-    const EE2_IDX : usize = 24;
-    const EE3_IDX : usize = 25;
-    const EE4_IDX : usize = 26;
-    const EE5_IDX : usize = 27;
-    const CE1_IDX : usize = 28;
-    const CE2_IDX : usize = 29;
-    const CE3_IDX : usize = 30;
-    const CE4_IDX : usize = 31;
-    const CE5_IDX : usize = 32;
-    const VARK_VIS_IDX : usize = 33;
-    const VARK_AUR_IDX : usize = 34;
-    const VARK_RW_IDX : usize = 35;
-    const VARK_KIN_IDX : usize = 36;
-    const MI_KIN_IDX : usize = 37;
-    const MI_EXIST_IDX : usize = 38;
-    const MI_INTER_IDX : usize = 39;
-    const MI_INTRA_IDX : usize = 40;
-    const MI_LOG_IDX : usize = 41;
-    const MI_MUS_IDX : usize = 42;
-    const MI_NAT_IDX : usize = 43;
-    const MI_VER_IDX : usize = 44;
-    const MI_VIS_IDX : usize = 45;
+mod bitmap;
 
-    fn new(students : usize) -> Self;
-    fn add_to_hyperedge(&mut self, student_idx : usize, hyperedge_idx : usize);
-    fn call_neighborghs_in_he(&self, student_idx : usize, hyperedge_idx : usize);
-    fn print(&self);
+use std::collections::HashMap;
+use bitmap::{BitmapLen, make_bitmap_of_len, resize_bitmap};
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{Result, Read, Write};
+use bincode::{serialize, deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Hyperedge {
+    #[serde(with = "BitmapLen")]
+    bitmap: Box<dyn BitmapLen>, // Bitmap representing the students in the hyperedge
 }
 
-impl Hypergraph for Vec<u64>
-{
-    fn new(students : usize) -> Vec<u64>
-    {
-        vec![0; students]
-    }
-
-    fn add_to_hyperedge(&mut self, student_idx : usize, hyperedge_idx : usize)
-    {
-        self[student_idx] |= 1 << hyperedge_idx;
-    }
-
-    fn call_neighborghs_in_he(&self, student_idx : usize, hyperedge_idx : usize)
-    {
-        let mask = 1 << hyperedge_idx;
-        for (idx, &he) in self.iter().enumerate() {
-            if idx != student_idx && (he & mask) != 0 {
-                // This is a neighbor in the same hyperedge
-                println!("Student {} is a neighbor of student {} in hyperedge {}", idx, student_idx, hyperedge_idx);
-            }
+impl Hyperedge {
+    // Crea una nueva hiperarista con un bitmap del tamaño adecuado para el número de estudiantes
+    pub fn new(size_bits: usize) -> Self {
+        let bitmap = make_bitmap_of_len(size_bits);
+        Hyperedge {
+            bitmap
         }
     }
 
-    fn print(&self) {
-        let mut mask = 8;
-        for index in 3..=45 {
-            print!("Hyperedge ");
-            match index
-            {
-                3 => println!("AMotiv1: "),
-                4 => println!("AMotiv2: "),
-                5 => println!("AMotiv3: "),
-                6 => println!("AMotiv4: "),
-                7 => println!("AMotiv5: "),
-                8 => println!("RMotiv1: "),
-                9 => println!("RMotiv2: "),
-                10 => println!("RMotiv3: "),
-                11 => println!("RMotiv4: "),
-                12 => println!("RMotiv5: "),
-                13 => println!("CMotiv1: "),
-                14 => println!("CMotiv2: "),
-                15 => println!("CMotiv3: "),
-                16 => println!("CMotiv4: "),
-                17 => println!("CMotiv5: "),
-                18 => println!("BEngage1: "),
-                19 => println!("BEngage2: "),
-                20 => println!("BEngage3: "),
-                21 => println!("BEngage4: "),
-                22 => println!("BEngage5: "),
-                23 => println!("EEngage1: "),
-                24 => println!("EEngage2: "),
-                25 => println!("EEngage3: "),
-                26 => println!("EEngage4: "),
-                27 => println!("EEngage5: "),
-                28 => println!("CEngage1: "),
-                29 => println!("CEngage2: "),
-                30 => println!("CEngage3: "),
-                31 => println!("CEngage4: "),
-                32 => println!("CEngage5: "),
-                33 => println!("VARK_VIS: "),
-                34 => println!("VARK_AUR: "),
-                35 => println!("VARK_RW: "),
-                36 => println!("VARK_KIN: "),
-                37 => println!("MI_KIN: "),
-                38 => println!("MI_EXIST: "),
-                39 => println!("MI_INTER: "),
-                40 => println!("MI_INTRA: "),
-                41 => println!("MI_LOG: "),
-                42 => println!("MI_MUS: "),
-                43 => println!("MI_NAT: "),
-                44 => println!("MI_VER: "),
-                45 => println!("MI_VIS: "),
-                 _=> (),
-            }
+    // Cambia el tamaño del bitmap de la hiperarista, ajustándolo al nuevo número de estudiantes
+    pub fn resize(&mut self, new_size_bits: usize) -> Result<(), String> {
+        self.bitmap = resize_bitmap(&mut self.bitmap, new_size_bits)?;
+        Ok(())
+    }
 
-            for student in 0..self.len() {
-                if (self[student] & mask)  != 0{
-                    println!("\t - Student {} ", student);
-                }
-            }
-            mask <<= 1;
+    // Agrega un estudiante a la hiperarista, estableciendo el bit correspondiente en el bitmap
+    pub fn add_student(&mut self, student_id: usize) -> Result<(), String> {
+        return self.bitmap.set_bit(student_id);
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Hypergraph {
+    hyperedges: HashMap<String, Hyperedge>, // Map from hyperedge name to Hyperedge
+    student_count: usize,                   // Number of students in the hypergraph
+}
+
+impl Hypergraph {
+    // Crea un nuevo hipergrafo con un número específico de estudiantes
+    pub fn new(student_count: usize) -> Self {
+        return Hypergraph {
+            hyperedges: HashMap::new(),
+            student_count,
+        };
+    }
+
+    // Crea una nueva hiperarista con los valores de los estudiantes seteado
+    pub fn add_hyperedge(&mut self, name: String, students: Vec<usize>) -> Result<(), String> {
+        let mut hyperedge = Hyperedge::new(self.student_count);
+        for student in students {
+            hyperedge.add_student(student)?;
         }
+        self.hyperedges.insert(name, hyperedge);
+        Ok(())
+    }
+
+    // Agrega un nuevo estudiante al hipergrafo, ajustando los bitmaps de las hiperaristas existentes
+    pub fn add_student(&mut self) -> Result<(), String> {
+        self.student_count += 1;
+        for hyperedge in self.hyperedges.values_mut() {
+            hyperedge.resize(self.student_count)?;
+        }
+        Ok(())
+    }
+
+    // Obtiene una referencia mutable a una hiperarista por su nombre, permitiendo modificarla
+    pub fn get_hyperedge_mut(&mut self, name: &str) -> Option<&mut Hyperedge> {
+        self.hyperedges.get_mut(name)
+    }
+
+    // Obtiene una referencia a una hiperarista por su nombre, permitiendo leerla
+    pub fn save_to_file(&self, filename: &str) -> Result<()> {
+        let encoded: Vec<u8> = serialize(self).unwrap();
+        let mut file = File::create(filename)?;
+        file.write_all(&encoded)?;
+        Ok(())
+    }
+
+    // Carga un hipergrafo desde un archivo, deserializando su contenido
+    pub fn load_from_file(filename: &str) -> Result<Self> {
+        let mut file = File::open(filename)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+        let hypergraph: Hypergraph = deserialize(&buffer).unwrap();
+        Ok(hypergraph)
     }
 }
