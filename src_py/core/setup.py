100072f26569f085b0864503e91bfd3659727fc3
DIRM2705 <dirm2705@gmail.com>
2026-06-02 19:08:02 -0600

    Remove unnecessary libraries from setup

--- src_py/core/setup.py
+++ src_py/core/setup.py
@@ -1,6 +1,5 @@
 import polars as pl
-from pathlib import Path
 
-def setup_experiment(hypergraph_path: Path):
+def setup_experiment():
     pl.Config.set_tbl_cols(-1)
     pl.Config.set_tbl_rows(-1)
