5ab49a7b27e5bc54beb236ab71e1afa097285fbe
DIRM2705 <dirm2705@gmail.com>
2026-07-10 08:26:06 -0600

    Add training for curriculum classifier

--- src_py/training/curriculum_classifier.py
+++ src_py/training/curriculum_classifier.py
@@ -6,125 +6,123 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 
 
-carpeta = Path(r"../data/curriculums")
+def train_curriculum_classifier(carpeta : Path):
+    # ==========================================================
+    # Crear filas
+    # ==========================================================
 
+    filas = []
 
-# ==========================================================
-# Crear filas
-# ==========================================================
+    for carpeta_clase in carpeta.iterdir():
 
-filas = []
-
-for carpeta_clase in carpeta.iterdir():
-
-    if not carpeta_clase.is_dir():
-        continue
-
-    clase = carpeta_clase.name
-
-    for archivo in carpeta_clase.glob("*.pdf"):
-
-        texto = extraer_texto_pdf(archivo)
-
-        if not texto.strip():
+        if not carpeta_clase.is_dir():
             continue
 
-        lemas = procesar_texto(texto)
+        clase = carpeta_clase.name
 
-        conteo = Counter(lemas)
+        for archivo in carpeta_clase.glob("*.pdf"):
 
-        # Evitar conflictos con nombres reservados
-        conteo.pop("clase", None)
-        conteo.pop("documento", None)
+            texto = extraer_texto_pdf(archivo)
 
-        fila = {
-            "__documento__": archivo.stem,
-            "__clase__": clase
-        }
+            if not texto.strip():
+                continue
 
-        fila.update(conteo)
+            lemas = procesar_texto(texto)
 
-        filas.append(fila)
+            conteo = Counter(lemas)
 
+            # Evitar conflictos con nombres reservados
+            conteo.pop("clase", None)
+            conteo.pop("documento", None)
 
-# ==========================================================
-# DataFrame
-# ==========================================================
+            fila = {
+                "__documento__": archivo.stem,
+                "__clase__": clase
+            }
 
-df = (
-    pl.DataFrame(filas)
-    .rename({
-        "__documento__": "documento",
-        "__clase__": "clase"
-    })
-    .fill_null(0)
-)
+            fila.update(conteo)
+
+            filas.append(fila)
 
 
+    # ==========================================================
+    # DataFrame
+    # ==========================================================
 
-# ==========================================================
-# Agrupar por clase
-# ==========================================================
-
-columnas = [
-    c for c in df.columns 
-    if c not in ("documento", "clase")
-]
-
-
-df_frecuencias = (
-    df
-    .group_by("clase")
-    .agg(
-        [
-            pl.col(c).sum().alias(c)
-            for c in columnas
-        ]
+    df = (
+        pl.DataFrame(filas)
+        .rename({
+            "__documento__": "documento",
+            "__clase__": "clase"
+        })
+        .fill_null(0)
     )
-    .sort("clase")
-)
-
-# ==========================================================
-# TF-IDF
-# ==========================================================
-
-X = df_frecuencias.drop("clase").to_numpy()
-
-transformer = TfidfTransformer()
-
-tfidf = transformer.fit_transform(X).toarray()
 
 
-df_tfidf = pl.DataFrame(
-    tfidf,
-    schema=df_frecuencias.drop("clase").columns
-)
+
+    # ==========================================================
+    # Agrupar por clase
+    # ==========================================================
+
+    columnas = [
+        c for c in df.columns 
+        if c not in ("documento", "clase")
+    ]
 
 
-df_tfidf = df_tfidf.insert_column(
-    0,
-    pl.Series(
-        "clase",
-        df_frecuencias["clase"]
+    df_frecuencias = (
+        df
+        .group_by("clase")
+        .agg(
+            [
+                pl.col(c).sum().alias(c)
+                for c in columnas
+            ]
+        )
+        .sort("clase")
     )
-)
 
-# ==========================================================
-# Matriz transpuesta
-# ==========================================================
+    # ==========================================================
+    # TF-IDF
+    # ==========================================================
 
-df_final = pl.DataFrame(
-    tfidf.T,
-    schema=df_frecuencias["clase"].to_list()
-)
+    X = df_frecuencias.drop("clase").to_numpy()
+
+    transformer = TfidfTransformer()
+
+    tfidf = transformer.fit_transform(X).toarray()
 
 
-df_final = df_final.insert_column(
-    0,
-    pl.Series(
-        "lema",
-        df_frecuencias.drop("clase").columns
+    df_tfidf = pl.DataFrame(
+        tfidf,
+        schema=df_frecuencias.drop("clase").columns
     )
-)
 
-df_final.write_parquet(r"models/curriculm_vectors")
+
+    df_tfidf = df_tfidf.insert_column(
+        0,
+        pl.Series(
+            "clase",
+            df_frecuencias["clase"]
+        )
+    )
+
+    # ==========================================================
+    # Matriz transpuesta
+    # ==========================================================
+
+    df_final = pl.DataFrame(
+        tfidf.T,
+        schema=df_frecuencias["clase"].to_list()
+    )
+
+
+    df_final = df_final.insert_column(
+        0,
+        pl.Series(
+            "lema",
+            df_frecuencias.drop("clase").columns
+        )
+    )
+
+    df_final.write_parquet(r"models/curriculm_vectors")
