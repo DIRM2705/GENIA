d14c9e0a8e04c8c93b7280441bd11e7fa9e43221
DIRM2705 <dirm2705@gmail.com>
2026-07-11 12:06:49 -0600

    Update optimize lematization

--- src_py/preprocessing/nlp.py
+++ src_py/preprocessing/nlp.py
@@ -7,35 +7,54 @@ from pathlib import Path
 
 nlp = None
 
+def token_valido(token : spacy.tokens.Token) -> bool:
+    """_summary_
+    Función que recibe una palabra y devuelve si es válida o no
+    Args:
+        palabra (str): Palabra a evaluar
+    Returns:
+        bool: True si es válida, False en caso contrario
+    """
+    return (token.is_alpha or token.is_digit) and not token.is_stop and not token.is_punct and not token.is_space and not token.like_url
+
+def normalizar_lema(lema : str) -> str:
+    """_summary_
+    Función que recibe un lema y devuelve su forma normalizada
+    Args:
+        lema (str): Lema a normalizar
+    Returns:
+        str: Lema normalizado
+    """
+    return normalize("NFKD", lema).encode("ASCII", "ignore").decode("utf-8").lower()
+
 # ==========================================================
 # Extraer texto
 # ==========================================================
 
-def _extraer_texto_pdf(ruta_pdf : Path) -> str:
+def _extraer_texto_pdf(ruta_pdf : Path) -> list[str]:
     texto = []
 
     with fitz.open(ruta_pdf) as doc:
         for pagina in doc:
             texto.append(pagina.get_text())
-
-    return " ".join(texto)
+            
+    return texto
 
 
 # ==========================================================
 # Procesar texto
 # ==========================================================
 
-def _procesar_texto(texto : str) -> list[str]:  
-    doc = nlp(texto)
-    lemmas = []
+def _procesar_texto(lineas : list[str]) -> list[str]: 
+    lemmas = set()
+    for texto in lineas:
+        texto = normalizar_lema(texto)
+        doc = nlp(texto)
+        lemmas.update(
+            normalizar_lema(token.lemma_) for token in doc if token_valido(token)
+        )
     
-    for token in doc:
-        if not token.is_stop and not token.is_punct and not token.is_space and not token.like_url:
-            lemmas.append(
-                normalize("NFKD", token.lemma_).encode("ASCII", "ignore").decode("utf-8").lower()
-            )
-            
-    return lemmas
+    return list(lemmas)
 
 def cargar_modelo_nlp():
     global nlp
