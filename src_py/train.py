23afc43ac22c4d006772afd1d181ce42db464b1a
DIRM2705 <dirm2705@gmail.com>
2026-07-11 13:10:25 -0600

    Update train to integrate model

--- src_py/train.py
+++ src_py/train.py
@@ -1,4 +1,4 @@
-from training.curriculum_classifier import train_curriculum_classifier
+from models.curriculum_classifier import CurriculumClassifier
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -6,9 +6,5 @@ import os
 if __name__ == "__main__":
     load_dotenv()
     training_set_path = Path(os.getenv("TRAINING_CURRICULUM_PATH"))
-    model = train_curriculum_classifier(training_set_path)
-    model.write_parquet(r"src_py\models\curriculm_vectors.parquet")
-    
-    print("Numero de lemas encontrados: ", model.shape)
-    
-    print(model.head(20))
+    model = CurriculumClassifier()
+    model.train(training_set_path)
