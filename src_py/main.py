8e64b0766f298bee34b983829d5af0107b85b21b
DIRM2705 <dirm2705@gmail.com>
2026-07-17 11:36:28 -0600

    Update synthetic data

--- src_py/main.py
+++ src_py/main.py
@@ -1,15 +1,13 @@
 from pathlib import Path
 import sys
 import polars as pl
-from experiments.genetic_algo import REAL_DATA_GA_EXPERIMENT
-from preprocessing.dataframe import load_from_csv
 from preprocessing.graficas import export_info_from_console
 
 pl.Config.set_tbl_cols(-1)
 pl.Config.set_tbl_rows(-1)
 
 if __name__ == "__main__":
-    df = load_from_csv("Pruebas1.csv")
+    df = pl.read_parquet(Path("src_py/data/synthetic_chars.parquet"))
     export_info_from_console(df)
     #REAL_DATA_GA_EXPERIMENT.run()
 
