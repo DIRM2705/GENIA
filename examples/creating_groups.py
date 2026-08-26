507a0706a41fae784153046f9ff531665a85fbb8
DIRM2705 <dirm2705@gmail.com>
2026-08-26 12:55:52 -0600

    Update documentation

--- examples/creating_groups.py
+++ examples/creating_groups.py
@@ -1,5 +1,5 @@
-from genia_libs import GeneticAlgorithm, hypergraph_from_dataframe
-from genia_libs.utils.dataframe import get_grouping_dataframe,load_preprocessed_lf
+from genia_libs import GeneticAlgorithm
+from genia_libs.utils.dataframe import load_preprocessed_lf
 import matplotlib.pyplot as plt
 import polars as pl
 
@@ -8,25 +8,28 @@ HYPERGRAPH_PATH = "data/test_data/PFGF1_hypergraph.hg"
 
     
 if __name__ == "__main__":
-    """_summary_
-        
-        Load the synthetic data of 400 students, create the hypergraph of features and perform the group formation process.
-    - GA parameters:
-    - Initial population: 30
-    - Maximum generations: 300000
-    - Spins per generation: 7
-    - Elitism: 2
-    - Mutation: 25% (Fixed)
-    - Crossover: 95%
-    - Number of groups to form: 16
+    """
+    Algoritmo genético para formar grupos a partir del hypergrafo.
+    
+    Parámetros:
+    - Población inicial: 30
+    - generaciones máximas: 300000
+    - Spins por generación: 7
+    - Elites: 2
+    - Tasa de mutación: 25% (Fija)
+    - Tasa de cruza: 95%
+    - Número de grupos a formar: 16
     """
 
-    ga = GeneticAlgorithm(30, 300000, 7, 2, 0.95, None) #Initial population, max generations, spins per generation, elitism, crossover probability, experiment file path
-    ga.show_config()
-    groups = ga.run(16, HYPERGRAPH_PATH) # Form 16 groups
+    #Configurar el algoritmo genético
+    ga = GeneticAlgorithm(30, 300000, 7, 2, 0.95, None) 
+    ga.show_config() #Mostrar la configuración del algoritmo genético
+    groups = ga.run(16, HYPERGRAPH_PATH) # Formar 16 grupos a partir del hypergrafo de características
         
     for group in groups:
-        df = load_preprocessed_lf(DATA_PATH).collect() #Load the preprocessed data of 400 students
-        group_df = df.filter(pl.col("Id").is_in(group))
+        df = load_preprocessed_lf(DATA_PATH).collect() #Obtiene el dataframe de características preprocesadas
+        group_df = df.filter(pl.col("Id").is_in(group)) #Filtra el dataframe para obtener solo los estudiantes que pertenecen al grupo actual
+        
+        # Mostrar los estudiantes que pertenecen al grupo actual
         print(f"Grupo {groups.index(group) + 1}:")
         print(group_df)
